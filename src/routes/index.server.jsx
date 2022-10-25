cf48007036a23b6e5b1d8f23208d15ce1e571d0e
DmitryDemid <dimon.demid15@gmail.com>
2022-10-24 22:32:37 -0400

    initializing

--- src/routes/index.server.jsx
+++ src/routes/index.server.jsx
@@ -1,28 +1,30 @@
-import { useShopQuery, CacheLong, gql } from "@shopify/hydrogen";
+import {useShopQuery, CacheLong, gql} from "@shopify/hydrogen";
 
 export default function Home() {
 
-  const data = useShopQuery({
-      query: SHOP_QUERY,
-      cache: CacheLong(),
-      preload: true,
-  });
+    const data = useShopQuery({
+        query: SHOP_QUERY,
+        cache: CacheLong(),
+        preload: true,
+    });
 
-const { data: { shop } } = data;
+    const {data: {shop}} = data;
 
-  return (
-    <div className="home-page container">
-        <h1>{shop.name}</h1>
-        <div>{shop.description}</div>
-    </div>
-  );
+    console.log(data)
+
+    return (
+        <div className="home-page container">
+            <h1>{shop.name}</h1>
+            <div>{shop.description}</div>
+        </div>
+    );
 }
 
 const SHOP_QUERY = gql`
-  query ShopInfo {
-    shop {
-      name
-      description
+    query ShopInfo {
+        shop {
+            name
+            description
+        }
     }
-  }
 `;
